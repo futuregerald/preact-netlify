c2e0036efe6be4f8918433866be59ea266b544b1
Gerald Onyango <gerald.onyango@gmail.com>
2020-04-30 10:00:12 -0400

    Update index.js

--- plugins/netlify-plugin-execute-go/index.js
+++ plugins/netlify-plugin-execute-go/index.js
@@ -2,7 +2,7 @@ exec = require('child_process').execFile;
 module.exports = {
     onPreBuild: () => {
       console.log('Hello, we will not execute this Go executable!')
-      const child = exec('hello', null, (err, stdout, stderr) => {
+      const child = exec('./hello', null, (err, stdout, stderr) => {
         if (err) {
             console.log(err)
           throw err;
